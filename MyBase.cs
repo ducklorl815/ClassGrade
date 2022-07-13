56c6b841e159260598aefeacc923c4fb6f4c9e3d
家毅 廖 <ducklorl815@gmail.com>
2022-07-13 17:21:30 +0800

    48

---
+++ MyBase.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyOwn
+{
+    internal class MyClass
+    {
+        //private string _ch_string;
+        public string name;
+        public int[] arr = new int[3];
+        public enum MyClassName
+        {
+            國文 = 0,
+            英文 = 1,
+            數學 = 2
+        }
+
+        int _sum;
+        public int sum
+        {
+            get
+            {
+                for(int i = 0; i < arr.Length; i++)
+                {
+                    _sum += arr[i]; 
+                }
+                return _sum;
+            }
+        }
+        public double avg
+        {
+            get
+            {
+                return Math.Round((double)_sum / arr.Length,2);
+            }
+        }
+        public string Max_class
+        {
+            get
+            {
+                string _Max_class= $"{(MyClassName)0}{arr[0]}";
+                int _Max = arr[0];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > _Max)
+                    {
+                        _Max_class = $"{(MyClassName)i}{arr[i]}";
+                        _Max = arr[i];
+                    }
+                }
+                return _Max_class;
+            }
+        }
+        public string min_class
+        {
+            get
+            {
+                string _min_class = $"{(MyClassName)0}{arr[0]}";
+                int _min = arr[0];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] < _min)
+                    {
+                        _min_class= $"{(MyClassName)i}{arr[i]}";
+                        _min = arr[i];
+                    }
+                }
+                return _min_class;
+            }
+        }
+
+        public override string ToString()
+        {
+            return 
+                $"{name,-8}"
+              + $"{arr[0],8}"
+              + $"{arr[1],11}"
+              + $"{arr[2],10}"
+              + $"{sum,11}"
+              + $"{avg,11}"
+              + $"{min_class,10}"
+              + $"{Max_class,9}"; 
+        }
+
+        public string na_string { set; get; }//------->接住字串  自動實作屬性
+        public string ch_string { set; get; }
+        public string en_string { set; get; }
+        public string ma_string { set; get; }
+        public int total;
+        public int ch   //------->輸出數字
+        {
+            get
+            {
+                if (!int.TryParse(ch_string, out int _ch))  //--->判斷字串是否是數字
+                {
+                    return -1;//--------> -1為異常數值
+                }
+                if (_ch > 100 || _ch < 0)
+                {
+                    return -2;//--------> -2為異常數值
+                }
+                else return _ch;
+            }
+        }
+        public int en   //------->輸出數字
+        {
+            get
+            {
+                if (!int.TryParse(en_string, out int _en))  //--->判斷字串是否是數字
+                {
+                    return -1;//--------> -1為異常數值
+                }
+                if (_en > 100 || _en < 0)
+                {
+                    return -2;//--------> -2為異常數值
+                }
+                else return _en;
+            }
+        }
+        public int ma   //------->輸出數字
+        {
+            get
+            {
+                if (!int.TryParse(ma_string, out int _ma))  //--->判斷字串是否是數字
+                {
+                    return -1;//--------> -1為異常數值
+                }
+                if (_ma > 100 || _ma < 0)
+                {
+                    return -2;//--------> -2為異常數值
+                }
+                else return _ma;
+            }
+        }
+
+
+
+
+    
+
+    }
+}
---
+++ MyGrade.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyOwn;
+
+namespace MyOwn
+{
+    public partial class MyGrade : Form
+    {
+        public MyGrade()
+        {
+            InitializeComponent();
+        }
+        List<MyClass> mylist = new List<MyClass>();
+        MyClass[] MC = new MyClass[300];
+        Random rnd=new Random();
+        int i;
+
+        private int student_count
+        {
+            get { return mylist.Count; }
+        }
+        private void btnRnd_Click(object sender, EventArgs e)
+        {
+            i = student_count;
+            MC[i] = new MyClass()
+            {
+                name = rnd.Next(101).ToString(),
+                arr = new int[] { rnd.Next(101), rnd.Next(0, 101), rnd.Next(0, 101) }
+            };
+            mylist.Add(MC[i]);
+            labName.Text += MC[i].ToString()+"\n";
+            labCount.Text = student_count.ToString();
+        }
+
+        private void btnRnd20_Click(object sender, EventArgs e)
+        {
+            for (int i =0; i<20; i++)
+            {
+                MC[i] = new MyClass()
+                {
+                    name = rnd.Next(101).ToString(),
+                    arr = new int[] { rnd.Next(101), rnd.Next(0, 101), rnd.Next(0, 101) }
+                };
+                labName.Text += MC[i].ToString() + "\n";
+                mylist.Add(MC[i]);
+            }
+ 
+            labCount.Text = student_count.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            mylist.Clear();
+            labName.Text = "";
+            labCount.Text = "";
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            MC[i] = new MyClass();
+            if (txtName.Text=="")
+            {
+                MessageBox.Show("請輸入姓名");
+                return;
+            }
+            MC[i].na_string = txtName.Text;
+            MC[i].ch_string = txtChinese.Text;
+            MC[i].en_string = txtEnglish.Text;
+            MC[i].ma_string = txtMath.Text;
+            if (MC[i].ch == -1 || MC[i].en == -1 || MC[i].ma == -1)
+            {
+                MessageBox.Show("字串輸入錯誤");
+                if (MC[i].ch == -1) txtChinese.Clear();
+                else if (MC[i].en == -1) txtEnglish.Clear();
+                else if (MC[i].ma == -1) txtMath.Clear();
+                return;
+            }
+            if (MC[i].ch == -2 || MC[i].en == -2 || MC[i].ma == -2)
+            {
+                MessageBox.Show("數字輸入錯誤");
+                if (MC[i].ch == -2) txtChinese.Clear();
+                else if (MC[i].en == -2) txtEnglish.Clear();
+                else if (MC[i].ma == -2) txtMath.Clear();
+                return;
+            }
+            i = student_count;
+            MC[i] = new MyClass()
+            {
+                name = txtName.Text,
+                arr = new int[] { int.Parse(txtChinese.Text), int.Parse(txtEnglish.Text), int.Parse(txtMath.Text) }
+            };
+            mylist.Add(MC[i]);
+            labName.Text += MC[i].ToString() + "\n";
+            labCount.Text = student_count.ToString();
+        }
+
+
+        void MyLayout()
+        {
+            int ch, en, ma;
+            labName.Text = "";
+            for (int i = 0; i < student_count; i++)
+            {
+                string na = mylist[i].na_string;
+                ch = mylist[i].ch;
+                en = mylist[i].en;
+                ma = mylist[i].ma;
+
+                int total = ch + en + ma;
+                string high = "";
+                int high_num = 0;
+                string low = "";
+                int low_num = 0;
+                double avg = Math.Round(((double)total) / 3, 2);
+
+
+                labName.Text += na + " " + ch + " " + en + " " + ma + "          " + (ch + en + ma) + "          " + ((ch + en + ma) / 3) + (low + low_num)+ (high + high_num)+ "\n";
+                //labName.Text += $"{na.PadRight(13)}{ch.ToString().PadRight(7)}{en.ToString().PadRight(7)}{ma.ToString().PadRight(7)}" +
+                //                $"{total.ToString().PadRight(7)}{avg.ToString().PadRight(7)}{(low + low_num).PadRight(7)}{(high + high_num).PadRight(0)}\n";
+            }
+            
+        }
+        private void label5_Layout(object sender, LayoutEventArgs e)
+        {
+            label5.Text = $"{"姓名".PadRight(9)}{"國文".PadRight(7)}{"英文".PadRight(7)}{"數學".PadRight(7)}" +
+                          $"{"總分".PadRight(7)}{"平均".PadRight(7)}{"最低".PadRight(7)}{"最高".PadRight(0)}";
+        }
+
+    }
+}
